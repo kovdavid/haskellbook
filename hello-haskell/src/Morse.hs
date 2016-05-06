b086cac731de0c2ec492f63aa865c6dcc038020b
Davs <kovdavid@gmail.com>
2016-05-06 16:14:58 +0200

    pull

---
+++ hello-haskell/src/Morse.hs
@@ -0,0 +1,12 @@
+module Morse
+       ( Morse
+       , charToMorse
+       , morseToChar
+       , stringToMorse
+       , letterToMorse
+       , morseToLetter
+       ) where
+
+main :: IO ()
+main = return ()
+
