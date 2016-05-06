81ba0a516149a8bc415d208d4a939367924be46e
Davs <kovdavid@gmail.com>
2016-05-06 16:14:37 +0200

    After hangman test

---
+++ hangman/src/Hangman.hs
@@ -0,0 +1,122 @@
+module Hangman where
+
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse, nub)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
+-- type WordList = [String]
+newtype WordList = WordList [String] deriving (Eq, Show)
+
+allWords :: IO WordList
+allWords = do
+  dict <- readFile "data/dict.txt"
+  return $ WordList (lines dict)
+
+minWordLength :: Int
+minWordLength = 5
+
+maxWordLength :: Int
+maxWordLength = 9
+
+gameWords :: IO WordList
+gameWords = do
+  (WordList aw) <- allWords
+  return $ WordList (filter gameLength aw)
+    where gameLength w =
+            let l = length (w :: String)
+             in l > minWordLength && l < maxWordLength
+
+randomWord :: WordList -> IO String
+randomWord (WordList wl) = do
+  randomIndex <- randomRIO (0, length wl - 1)
+  return $ wl !! randomIndex
+
+randomWord' :: IO String
+randomWord' = gameWords >>= randomWord
+
+data Puzzle = Puzzle String [Maybe Char] [Char] deriving (Eq)
+
+instance Show Puzzle where
+  show (Puzzle _ discovered guessed) =
+    (intersperse ' ' $ fmap renderPuzzleChar discovered)
+    ++ " Guessed so far: " ++ guessed
+
+freshPuzzle :: String -> Puzzle
+freshPuzzle str = Puzzle str (map (const Nothing) str) []
+
+charInWord :: Puzzle -> Char -> Bool
+charInWord (Puzzle str _ _) c = elem c str
+
+alreadyGuessed :: Puzzle -> Char -> Bool
+alreadyGuessed (Puzzle _ _ guessed) c = elem c guessed
+
+renderPuzzleChar :: Maybe Char -> Char
+renderPuzzleChar Nothing  = '_'
+renderPuzzleChar (Just c) = c
+
+fillInCharacter :: Puzzle -> Char -> Puzzle
+fillInCharacter (Puzzle word filledInSoFar s) c =
+  Puzzle word newFilledInSoFar (c : s)
+    where zipper wordChar guessChar =
+            if wordChar == c
+            then Just wordChar
+            else guessChar
+          newFilledInSoFar =
+            zipWith zipper word filledInSoFar
+
+maybeToList :: [Maybe a] -> [a]
+maybeToList xs = foldr go [] xs
+  where go Nothing acc = acc
+        go (Just x) acc = x:acc
+
+handleGuess :: Puzzle -> Char -> IO Puzzle
+handleGuess puzzle guess = do
+  putStrLn $ "Your guess was: " ++ [guess]
+  case (charInWord puzzle guess, alreadyGuessed puzzle guess) of
+    (_, True) -> do
+      putStrLn "You already guessed that character"
+      return puzzle
+    (True, _) -> do
+      putStrLn "Good guess"
+      return (fillInCharacter puzzle guess)
+    (False, _) -> do
+      putStrLn "Bad guess"
+      return (fillInCharacter puzzle guess)
+
+gameOver :: Puzzle -> IO ()
+gameOver (Puzzle wordToGuess maybeChars guessed) =
+  let numGuessed = length guessed
+      numGoodGuesses = length . nub . maybeToList $ maybeChars
+      numBadGuesses = numGuessed - numGoodGuesses
+      wordLength = length wordToGuess
+   in if numGoodGuesses /= wordLength && numBadGuesses > wordLength then
+    do putStrLn "You lost!"
+       putStrLn $ "The word was: " ++ wordToGuess
+       exitSuccess
+  else return ()
+
+gameWin :: Puzzle -> IO ()
+gameWin (Puzzle _ filledInSoFar _) =
+  if all isJust filledInSoFar then
+    do putStrLn "You win!"
+       exitSuccess
+  else return ()
+
+runGame :: Puzzle -> IO ()
+runGame puzzle = forever $ do
+  gameOver puzzle
+  gameWin puzzle
+  putStrLn $ "Current puzzle: " ++ show puzzle
+  putStr $ "Guess: "
+  guess <- getLine
+  case guess of
+    [c] -> handleGuess puzzle c >>= runGame
+    _   -> putStrLn "Enter a single char!"
+
+hangmanMain = do
+  word <- randomWord'
+  let puzzle = freshPuzzle (fmap toLower word)
+  runGame puzzle
--- hangman/test/Spec.hs
+++ hangman/test/Spec.hs
@@ -1,6 +1,34 @@
-module Test where
+module Main where
 
-
+import Test.Hspec
+import Hangman
 
 main :: IO ()
-main = return ()
+main = hspec $ do
+  describe "fillInCharacter" $ do
+    it "good guess" $ do
+      let guessChar = 'd'
+          p  = Puzzle "davs" [Nothing, Nothing, Nothing, Nothing] []
+          p' = Puzzle "davs" [Just guessChar, Nothing, Nothing, Nothing] [guessChar]
+       in fillInCharacter p guessChar `shouldBe` p'
+    it "bad guess" $ do
+      let guessChar = 'x'
+          p  = Puzzle "davs" [Nothing, Nothing, Nothing, Nothing] []
+          p' = Puzzle "davs" [Nothing, Nothing, Nothing, Nothing] [guessChar]
+       in fillInCharacter p guessChar `shouldBe` p'
+
+  describe "handleGuess" $ do
+    it "good guess" $ do
+      let guessChar = 'd'
+          p  = Puzzle "davs" [Nothing, Nothing, Nothing, Nothing] []
+          p' = Puzzle "davs" [Just guessChar, Nothing, Nothing, Nothing] [guessChar]
+       in do
+         resultPuzzle <- handleGuess p guessChar
+         resultPuzzle `shouldBe` p'
+    it "bad guess" $ do
+      let guessChar = 'x'
+          p  = Puzzle "davs" [Nothing, Nothing, Nothing, Nothing] []
+          p' = Puzzle "davs" [Nothing, Nothing, Nothing, Nothing] [guessChar]
+       in do
+         resultPuzzle <- handleGuess p guessChar
+         resultPuzzle `shouldBe` p'
