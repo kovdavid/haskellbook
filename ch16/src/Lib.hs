48f97c43f9442249b13a596e2ed60aab0ca80a72
Davs <kovdavid@gmail.com>
2016-05-08 19:59:32 +0200

    Functors

--- ch16/src/Lib.hs
+++ ch16/src/Lib.hs
@@ -4,3 +4,35 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+replaceWithP :: b -> Char
+replaceWithP = const 'p'
+
+lms :: [Maybe String]
+lms = [Just "ave", Nothing, Just "woohoo"]
+
+replaceWithP' :: [Maybe [Char]] -> Char
+replaceWithP' = replaceWithP
+
+liftedReplace :: Functor f => f a -> f Char
+liftedReplace = fmap replaceWithP
+
+liftedReplace' :: [Maybe [Char]] -> [Char]
+liftedReplace' = liftedReplace
+
+twiceLifted :: (Functor f1, Functor f) =>
+               f (f1 a) -> f (f1 Char)
+twiceLifted = (fmap . fmap) replaceWithP
+
+twiceLifted' :: [Maybe [Char]] -> [Maybe Char]
+twiceLifted' = twiceLifted
+
+--          fmap        fmap
+-- (.) :: (b -> c) -> (a -> b) -> a -> c
+
+-- fmap1 :: Functor f => (m -> n) -> f m -> f n
+-- fmap2 :: Functor g => (x -> y) -> g x -> g y
+
+-- (fmap1 . fmap2)
+
+-- (m -> y) -> f (g m) -> f (g y)
