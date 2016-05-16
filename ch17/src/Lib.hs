645780ffe39ce7b58040fc94b6be7d41726c75f7
Davs <kovdavid@gmail.com>
2016-05-16 08:21:01 +0200

    Applicatives WIP

---
+++ ch17/src/Lib.hs
@@ -0,0 +1,62 @@
+module Lib where
+
+import Data.List (elemIndex)
+import Data.Monoid ((<>))
+
+-- class Functor f => Applicative f where
+--   pure :: a -> f a
+--   (<*>) :: f (a -> b) -> f a -> f b
+
+added :: Maybe Integer
+added = (+3) <$> (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
+
+y :: Maybe Integer
+y = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
+
+z :: Maybe Integer
+z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
+
+tupled :: Maybe (Integer, Integer)
+tupled = (,) <$> y <*> z
+
+x :: Maybe Int
+x = elemIndex 3 [1..5]
+
+x' :: Maybe Int
+x' = elemIndex 4 [1..5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = max' <$> x <*> x'
+
+xs = [1, 2, 3]
+ys = [4, 5, 6]
+
+xx :: Maybe Integer
+xx = lookup 3 $ zip xs ys
+
+yy :: Maybe Integer
+yy = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = fmap sum $ (,) <$> xx <*> yy
+
+newtype Identity a = Identity a deriving (Eq, Ord, Show)
+
+instance Functor Identity where
+  fmap f (Identity v) = Identity (f v)
+
+instance Applicative Identity where
+  pure = Identity
+  (<*>) (Identity f) (Identity v) = Identity (f v)
+
+newtype Constant a b = Constant { getConstant :: a } deriving (Eq, Ord, Show)
+
+instance Functor (Constant a) where
+  fmap _ (Constant v) = Constant v
+
+instance Monoid a => Applicative (Constant a) where
+  pure _ = Constant mempty
+  (<*>) (Constant v) (Constant w) = Constant $ v <> w
