08fcdcd5e8e49882d0e940ec42ca6849d5c3fe83
Davs <kovdavid@gmail.com>
2016-05-26 16:20:50 +0200

    Maybe Monad

--- ch18/src/Lib.hs
+++ ch18/src/Lib.hs
@@ -1,12 +1,55 @@
-module Lib
-    ( someFunc
-    , bind
-    ) where
+module Lib where
 
 import Control.Monad (join)
+import Control.Applicative ((*>))
 
-bind :: Monad m => (a -> m b) -> m a -> m b
-bind f v = join $ fmap f v
+-- let andOne x = [x , 1]
+--
+-- fmap andOne [4, 5, 6]
+-- concat $ fmap andOne [4, 5, 6]
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+-- _ = join :: Monad m => m (m a) -> m a
+
+-- bind :: Monad m => (a -> m b) -> m a -> m b
+-- bind f v = join $ fmap f v
+
+sequencing :: IO ()
+sequencing = do
+  putStrLn "foo"
+  putStrLn "bar"
+
+sequencing' :: IO ()
+sequencing' = do
+  putStrLn "foo" >> putStrLn "bar"
+
+sequencing'' :: IO ()
+sequencing'' = do
+  putStrLn "foo" *> putStrLn "bar"
+
+binding :: IO ()
+binding = do
+  name <- getLine
+  putStrLn name
+
+binding' :: IO ()
+binding' = do
+  getLine >>= putStrLn
+
+bindAndSequence :: IO ()
+bindAndSequence = do
+  putStrLn "name pls:"
+  name <- getLine
+  putStrLn ("hello: " ++ name)
+
+bindAndSequence' :: IO ()
+bindAndSequence' = do
+  putStrLn "name pls:"
+  >> getLine
+  >>= \name -> putStrLn ("hello: " ++ name)
+
+twiceWhenEven :: [Integer] -> [Integer]
+twiceWhenEven xs = do
+  x <- xs
+  if even x
+    then [x*x, x*x]
+    else []
