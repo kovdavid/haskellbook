0bbe2f9ee98399627a1f2d7e5cac26c9dac5a9e8
Davs <kovdavid@gmail.com>
2016-06-05 11:27:13 +0200

    After Foldable

---
+++ ch20/src/ChapterExercises.hs
@@ -0,0 +1,65 @@
+module ChapterExercises where
+
+import Data.Monoid
+
+data Constant a b = Constant a deriving (Show)
+
+instance Foldable (Constant a) where
+  foldMap _ _ = mempty
+  foldr _ acc _ = acc
+
+-----------------------------------------------------
+
+data Two a b = Two a b deriving (Show)
+
+instance Foldable (Two a) where
+  foldMap f (Two _ x) = f x
+  foldr f acc (Two _ x) = f x acc
+
+-----------------------------------------------------
+
+data Three a b c = Three a b c deriving (Show)
+
+instance Foldable (Three a b) where
+  foldMap f (Three _ _ x) = f x
+  foldr f acc (Three _ _ x) = f x acc
+
+-----------------------------------------------------
+
+data Three' a b = Three' a b b deriving (Show)
+
+instance Foldable (Three' a) where
+  foldMap f (Three' _ x y) = f x <> f y
+  foldr f acc (Three' _ x y) = f x $ f y acc
+
+-----------------------------------------------------
+
+data Four' a b = Four' a b b b deriving (Show)
+
+instance Foldable (Four' a) where
+  foldMap f (Four' _ x y z) = f x <> f y <> f z
+  foldr f acc (Four' _ x y z) = f x $ f y $ f z acc
+
+-----------------------------------------------------
+
+filterF :: (Applicative f, Foldable t, Monoid (f a))
+        => (a -> Bool) -> t a -> f a
+filterF f xs = foldMap go xs
+  where go x
+          | f x == True = pure x
+          | otherwise = mempty
+
+main :: IO ()
+main = do
+  putStrLn "====== Constant a b"
+  print $ foldMap id (Constant 5 :: Constant Int [Int])
+  print $ foldMap (:[]) (Two 5 7 :: Two Int Int)
+  print $ foldMap (Sum) (Three 0 5 7 :: Three Int Int Int)
+  print $ foldMap (Sum) (Three' 0 5 7 :: Three' Int Int)
+  print $ foldMap (Product) (Four' 9 3 5 7 :: Four' Int Int)
+  print $ foldr (+) 0 (Constant 5 :: Constant Int Int)
+  print $ foldr (:) [] (Two 5 7 :: Two Int Int)
+  print $ foldr (+) 0 (Three 0 5 7 :: Three Int Int Int)
+  print $ foldr (+) 0 (Three' 0 5 7 :: Three' Int Int)
+  print $ foldr (*) 1 (Four' 9 3 5 7 :: Four' Int Int)
+  print $ (filterF even [1..5] :: [Int])
--- ch20/src/Lib.hs
+++ ch20/src/Lib.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE ScopedTypeVariables #-}
-
 module Lib where
 
-import Data.Foldable
+-- import Data.Foldable
 import Data.Monoid
 
 data Identity a = Identity a
@@ -30,9 +28,81 @@ instance Foldable Optional where
 --   foldMap :: Monoid m => (a -> m) -> t a -> m
 --   fold :: Monoid m => t m -> m
 
-hole = undefined
-data Hole = Hole
+sum :: (Foldable t, Num a) => t a -> a
+sum = getSum . Lib.foldMap Sum
 
-sum :: forall t a . (Foldable t, Num a) => t a -> a
-sum xs = undefined
-  where _ = xs :: (Foldable t, Num a) => t a
+product :: (Foldable t, Num a) => t a -> a
+product = getProduct . Lib.foldMap Product
+
+elem :: (Foldable t, Eq a) => a -> t a -> Bool
+elem key values = getAny $ Lib.foldMap (Any . (== key)) values
+
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' key values = foldr go False values
+  where go _ True = True
+        go v False = v == key
+
+minimum :: (Foldable t, Ord a) => t a -> Maybe a
+minimum = foldr go Nothing
+  where go x Nothing = Just x
+        go x (Just acc)
+          | x `compare` acc == LT = Just x
+          | otherwise = Just acc
+
+maximum :: (Foldable t, Ord a) => t a -> Maybe a
+maximum = foldr go Nothing
+  where go x Nothing = Just x
+        go x (Just acc)
+          | x `compare` acc == GT = Just x
+          | otherwise = Just acc
+
+null :: (Foldable t) => t a -> Bool
+null = foldr (\_ _ -> False) True
+
+length :: (Foldable t) => t a -> Int
+length = foldr (\_ acc -> acc + 1) 0
+
+toList :: (Foldable t) => t a -> [a]
+toList = foldr (:) []
+
+fold :: (Foldable t, Monoid m) => t m -> m
+fold = Lib.foldMap id
+
+foldMap :: (Foldable t, Monoid m) => (a -> m) -> t a -> m
+foldMap f xs = foldr (\x acc -> f x <> acc) mempty xs
+
+main :: IO ()
+main = do
+  putStrLn "Lib.sum"
+  putStrLn $ (++) "    " $ show $ Lib.sum ([1..4] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.sum ([] :: [Integer])
+  putStrLn "Lib.product"
+  putStrLn $ (++) "    " $ show $ Lib.product ([1..4] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.product ([] :: [Integer])
+  putStrLn "Lib.elem"
+  putStrLn $ (++) "    " $ show $ Lib.elem 3 ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.elem 3 ([1..4] :: [Integer])
+  putStrLn "Lib.elem'"
+  putStrLn $ (++) "    " $ show $ Lib.elem' 3 ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.elem' 3 ([1..4] :: [Integer])
+  putStrLn "Lib.minimum"
+  putStrLn $ (++) "    " $ show $ Lib.minimum ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.minimum ([1..4] :: [Integer])
+  putStrLn "Lib.maximum"
+  putStrLn $ (++) "    " $ show $ Lib.maximum ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.maximum ([1..4] :: [Integer])
+  putStrLn "Lib.null"
+  putStrLn $ (++) "    " $ show $ Lib.null ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.null ([1..4] :: [Integer])
+  putStrLn "Lib.length"
+  putStrLn $ (++) "    " $ show $ Lib.length ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.length ([1..4] :: [Integer])
+  putStrLn "Lib.toList"
+  putStrLn $ (++) "    " $ show $ Lib.toList ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.toList ([1..4] :: [Integer])
+  putStrLn "Lib.fold"
+  putStrLn $ (++) "    " $ show $ Lib.fold ([] :: [Sum Integer])
+  putStrLn $ (++) "    " $ show $ Lib.fold ([Sum 1, Sum 2] :: [Sum Integer])
+  putStrLn "Lib.foldMap"
+  putStrLn $ (++) "    " $ show $ Lib.foldMap Sum ([] :: [Integer])
+  putStrLn $ (++) "    " $ show $ Lib.foldMap Sum ([1..4] :: [Integer])
