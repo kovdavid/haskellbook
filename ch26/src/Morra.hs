3725831608a1a37e82b43918d0f02312d06c1e93
Davs <kovdavid@gmail.com>
2016-10-02 18:54:29 +0200

    Morra: add empty file

---
+++ ch26/src/Morra.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+module Morra where
+
+import Control.Monad
+import Control.Applicative
+
+main :: IO ()
+main = do
+  return ()
+
