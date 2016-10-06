6ec997efc461f398498a0379f1ddb3be4ef656a5
Davs <kovdavid@gmail.com>
2016-10-06 16:19:52 +0200

    2016-10-06 16:19:52

--- ch26/src/Morra.hs
+++ ch26/src/Morra.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE InstanceSigs #-}
+
 module Morra where
 
 import Control.Monad
@@ -13,20 +14,37 @@ import System.Random
 import qualified Text.Read as TR
 import qualified Data.Map as M
 
-data Choice = Odds | Evens | Exit deriving (Eq, Ord, Show)
-data Player = Player | CPU deriving (Eq, Show)
+data Choice
+  = Odds
+  | Evens
+  | Exit
+  deriving (Eq, Ord, Show)
+
+data Player
+  = Player
+  | CPU
+  deriving (Eq, Show)
 
 data Score = Score
   { playerScore :: Integer
   , computerScore :: Integer
-  } deriving Show
+  } deriving (Show)
 
 data GameState = GameState
   { gameScore :: Score
-  , gameSeed :: StdGen
+  , gameRound :: Integer
   , player3grams :: M.Map (Integer, Integer) Integer
   , lastPlayerNumbers :: (Maybe Integer, Maybe Integer)
-  } deriving Show
+  } deriving (Show)
+
+initialGameState :: GameState
+initialGameState =
+  GameState
+  { gameScore = Score 0 0
+  , gameRound = 0
+  , player3grams = M.empty
+  , lastPlayerNumbers = (Nothing, Nothing)
+  }
 
 validateChoice :: Monad m => String -> m (Maybe Choice)
 validateChoice "o" = return $ Just Odds
@@ -50,30 +68,34 @@ askPlayerNumber = do
   case mInt of
     Just num ->
       if num >= 0
-      then return num
-      else putStr "Input must be positive. " >> askPlayerNumber
+        then return num
+        else putStr "Input must be positive. " >> askPlayerNumber
     Nothing -> putStr "Wrong input. " >> askPlayerNumber
-
-getComputerChoice :: Choice -> Choice
-getComputerChoice Odds = Evens
-getComputerChoice Evens = Odds
 
 main :: IO ()
 main = do
-  let gameState = GameState (Score 0 0) (mkStdGen 0)
-  (_, finalGameState) <- runStateT mainLoop gameState
+  (_, finalGameState) <- runStateT mainLoop initialGameState
   print $ show $ finalGameState
   putStrLn "Bye"
 
 getWinner :: Integer -> Choice -> Player
 getWinner sum player1Choice = go (odd sum) player1Choice
-  where go True Odds = Player
-        go False Evens = Player
-        go _ _ = CPU
+  where
+    go True Odds = Player
+    go False Evens = Player
+    go _ _ = CPU
 
 updateGameScore :: Score -> Player -> Score
-updateGameScore (Score p c) Player = Score (p+1) c
-updateGameScore (Score p c) CPU = Score p (c+1)
+updateGameScore (Score p c) Player = Score (p + 1) c
+updateGameScore (Score p c) CPU = Score p (c + 1)
+
+increaseGameRound :: GameState -> GameState
+increaseGameRound state = state { gameRound = (gameRound state) + 1 }
+
+savePlayerNumber :: Integer -> GameState -> GameState
+savePlayerNumber num_new state =
+  let (_, num_old) = lastPlayerNumbers state
+   in state { lastPlayerNumbers = (num_old, Just num_new)}
 
 mainLoop :: StateT GameState IO ()
 mainLoop = do
@@ -81,19 +103,22 @@ mainLoop = do
     playerChoice <- lift $ askPlayerChoice
 
     when (playerChoice /= Exit) $ do
-        let computerChoice = getComputerChoice playerChoice
+        modify increaseGameRound
+
         playerNumber <- lift $ askPlayerNumber
-        let (computerNumber, newGameSeed) = randomR (0, playerNumber+1) $ gameSeed gameState
+
+        computerNumber <- lift $ randomRIO (0, playerNumber+1)
 
         let winner = getWinner (playerNumber + computerNumber) playerChoice
         let newGameScore = updateGameScore (gameScore gameState) winner
 
-        put $ GameState newGameScore newGameSeed
+        modify $ savePlayerNumber playerNumber
 
-        liftIO $ print $ "PlayerChoice: " ++ show playerChoice
-        liftIO $ print $ "PlayerNumber: " ++ show playerNumber
-        liftIO $ print $ "ComputerChoice: " ++ show computerChoice
-        liftIO $ print $ "ComputerNumber: " ++ show computerNumber
-        liftIO $ print $ "Winner: " ++ show winner
+        -- put $ GameState newGameScore 0 M.empty (Nothing, Nothing)
 
         mainLoop
+
+        -- liftIO $ print $ "PlayerChoice: " ++ show playerChoice
+        -- liftIO $ print $ "PlayerNumber: " ++ show playerNumber
+        -- liftIO $ print $ "ComputerNumber: " ++ show computerNumber
+        -- liftIO $ print $ "Winner: " ++ show winner
