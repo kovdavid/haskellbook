fccefd7904b18b47fa45c524fcf1bf51ccf21385
Davs <kovdavid@gmail.com>
2016-10-08 10:09:29 +0200

    2016-10-08 10:09:29

--- ch26/src/MonadTrans.hs
+++ ch26/src/MonadTrans.hs
@@ -7,6 +7,19 @@ module MonadTrans where
 import Web.Scotty
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
+import Control.Monad.Trans.Reader
+
+val :: ReaderT Int (ReaderT Char IO) Double
+val = ReaderT $ \x ->
+        ReaderT $ \y -> do
+          return 1.0
+
+val' :: ReaderT Int (ReaderT Char IO) Double
+val' = do
+  x :: Int <- ask
+  y :: Char <- lift $ ask
+  return 1.0
+
 
 main :: IO ()
 main = scotty 3000 $ do
