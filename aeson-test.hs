eb87bbdbea3c25afdabca0089bea2c281a4a24ab
Davs <kovdavid@gmail.com>
2016-10-09 23:23:47 +0200

    add aeson-test

---
+++ aeson-test.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Main where
+
+import Data.Aeson
+import Data.Aeson.Types
+import GHC.Generics
+
+data Test1 = Test1
+  { _name :: String
+  , _age :: Integer
+  } deriving (Show, Generic)
+
+data Test2 = Test2
+  { _id :: Integer
+  , _people :: [Test1]
+  } deriving (Show, Generic)
+
+json_data1 = "{\"name\":\"name1\",\"age\":10}"
+json_data = "{\"id\":4,\"people\":[{\"name\":\"name1\",\"age\":10},{\"name\":\"name2\",\"age\":12}]}"
+
+instance FromJSON Test1 where
+  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 1 }
+instance FromJSON Test2 where
+  parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 1 }
+
+main :: IO ()
+main = do
+  let x :: Maybe Test2 =  decode json_data
+  print $ show $ x
+  return ()
+
