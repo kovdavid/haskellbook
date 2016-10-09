9ae507731046a376fa1d435c6a984b8c64cbef03
Davs <kovdavid@gmail.com>
2016-10-09 19:22:32 +0200

    2016-10-09 19:22:32

--- ch27/src/Main.hs
+++ ch27/src/Main.hs
@@ -6,6 +6,7 @@ module Main where
 
 import Control.Monad
 import Control.Applicative
+import Debug.Trace
 
 data Test
   = A Test2
@@ -29,6 +30,17 @@ forceTest2 (A (C i)) = i
 forceTest2 (A (D i)) = i
 forceTest2 (B (C i)) = i
 forceTest2 (B (D i)) = i
+
+inc = (+ 1)
+
+twice = inc . inc
+
+howManyTimes =
+  inc (trace "I got eval'd" (1 + 1)) + twice (trace "I got eval'd" (1 + 1))
+
+howManyTimes' =
+  let onePlusOne = trace "I got eval'd" (1 + 1)
+  in inc onePlusOne + twice onePlusOne
 
 main :: IO ()
 main = do
