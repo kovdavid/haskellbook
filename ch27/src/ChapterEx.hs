d68020397a2fb360d2a8a9c513efb85d7b8015c8
Davs <kovdavid@gmail.com>
2016-11-18 17:17:16 +0100

    2016-11-18 17:17:16

---
+++ ch27/src/ChapterEx.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE BangPatterns #-}
+module ChapterEx where
+
+data List a =
+  Nil |
+  Cons a (List a)
+  deriving Show
+
+take' n _ | n <= 0 = Nil
+take' _ Nil = Nil
+take' n (Cons x xs) = (Cons x (take' (n-1) xs))
+
+map' _ Nil = Nil
+map' f (Cons x xs) = (Cons (f x) (map' f xs))
+
+repeat' x = xs where xs = (Cons x xs)
+
+x = undefined
+y = x `seq` "blah"
+
+main = do
+  -- print $ take' 10 $ map' (+1) (repeat' 1)
+  print $ snd $ (x, y)
+
